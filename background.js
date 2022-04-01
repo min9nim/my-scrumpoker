0cdce8e57e6134f9189c7bd99abb2240a289614b
keating <min9nim@gmail.com>
2022-04-01 23:37:24 +0900

    💬 Add or update text and literals

--- background.js
+++ background.js
@@ -1,10 +1,7 @@
 // background.js
 
-let color = '#3aa757';
-
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cgreen', `color: ${color}`);
+  console.log('my-scrumpoker installed')
 });
 
 
