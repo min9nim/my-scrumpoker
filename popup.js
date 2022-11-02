8c907b50dde5800664fc8f0c3ceff6e9b8311d6c
Keating <min9nim@gmail.com>
2022-11-02 14:36:59 +0900

    update

--- popup.js
+++ popup.js
@@ -13,10 +13,11 @@ btn.addEventListener("click", async () => {
 // The body of this function will be executed as a content script inside the
 // current page
 function setPoints() {
-  // document.querySelector('#dkHead')?.remove()
   document.querySelector('.area_menu')?.remove()
   var dom = document.querySelector('#dkHead')
-  dom?.setAttribute('style', 'background-color: rgba(255,255,255,.45);border-bottom: 0')
+  dom.style['border-bottom'] = '0'
+  dom.style['background-color'] = 'rgba(255,255,255,.05)'
+  dom.style.left = '-16px'
 
   setInterval(() => {
     document.querySelectorAll('table.mat-table div').forEach(item => {
